395b2f69fbbb77c376da9123aae6b2d8aa55bf71
Shawn Duong <shawnduong@protonmail.com>
2020-06-14 16:54:56 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# UCM-RegE
-UC Merced Registration Eye: a Python script that helps students get the classes they need by alerting them when seats open up.
+# UC Merced Registration Eye
+
+This is a Python script that helps UC Merced students get the classes they need by alerting them when seats open up. This script works by reading a list of CRNs (course reference numbers) from the `courses.list` file, getting the URLs for the respective courses from the saved `page.html` file (last updated April 2020), and then scraping the online course page to see if there are any open seats. If there are open seats, the program will play a simple alert sound.
+
+It's recommended that you let this run on your computer 24/7 in the background while you do other things. Whenever you hear the alert sound, you can check back on the script to see which class is open. This script requires a stable and constant internet connection. If this is interrupted at any time then the script will not function and you will have to restart it when you regain a connection.
+
+## Installation
+
+Installation is fairly simple. Just clone the git repository and run the `script.py` Python script. Make sure that you have the following dependencies installed:
+
+- `bs4`
+- `multiprocessing`
+- `playsound`
+- `requests`
+- `time`
+
+## Setup
+
+Replace the sample CRNs in `courses.list` with the CRNs of the courses that you need.
+
+## Testing
+
+This has been tested on a custom Arch Linux system with Python 3.8.2. It is not known whether or not this script will function in another environment such as Windows or Mac OS. Please submit an issue if this script does not work for any reason, complete with a description, error logs, steps to reproduce, and other information you deem relevant.
---
+++ script.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import bs4
+import multiprocessing
+import playsound
+import requests
+import time
+
+def read_courses() -> list:
+
+	try:
+		with open("courses.list", "r") as f:
+			courses = [line.strip("\n") for line in f.readlines()]
+	except:
+		print(":: Error reading courses.list.")
+		exit(-1)
+
+	print(":: The following courses will be monitored:")
+
+	for crn in courses:
+		print(":: CRN %s" % crn)
+
+	print()
+
+	return courses
+
+def get_course_urls(courses: list) -> dict:
+
+	try:
+		with open("page.html", "r") as f:
+			data = f.read()
+	except:
+		print(":: Trouble reading offline catalog.")
+		exit(-1)
+
+	soup = bs4.BeautifulSoup(data, "html.parser")
+
+	courses_crns = {}
+
+	links = soup.find_all("a")
+
+	for link in links:
+		if link.text in courses:
+			courses_crns[link.text] = link["href"]
+
+	return courses_crns
+
+def get_open_seats(url: str) -> str:
+
+	page = requests.get(url).content
+	soup = bs4.BeautifulSoup(page, "html.parser")
+	table = soup.find_all("table", attrs={"class": "datadisplaytable"})[-1]
+	seats = table.find_all("td", attrs={"class": "dddefault"})[-1].text
+	opens = True if int(seats) > 0 else False
+
+	if opens:
+		seats = "\033[31;1m%s <==\033[0;0m" % seats
+		playsound.playsound("alert.ogg")
+
+	return seats
+
+def check_courses(courses: dict) -> None:
+
+	print(":: Checking courses.")
+	print(":: TIME: %s" % time.strftime("%d %b %Y @ %H:%M:%S"))
+
+	wPool = multiprocessing.Pool(4)
+	seats = wPool.map(get_open_seats, list(courses.values()))
+	wPool.close()
+	wPool.join()
+
+	c = 0
+
+	for crn in courses.keys():
+		print(":: CRN %s: OPEN=%s" % (crn, seats[c]))
+		c += 1
+
+	print()
+
+def main() -> None:
+
+	print("Registration Eye (v. Fall 2020)")
+	print("Author: Shawn Duong, aka IrisCS s.kat.")
+	print("Please see the README.md for setup instructions!")
+	print()
+
+	courses = read_courses()
+	courses = get_course_urls(courses)
+
+	while True:
+		try:
+			check_courses(courses)
+			time.sleep(10)
+		except KeyboardInterrupt:
+			print("\r:: Interrupt detected. Exiting.")
+			exit(0)
+
+if __name__ == "__main__":
+	main()
